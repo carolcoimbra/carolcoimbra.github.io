34bb95cad7fc6576f5318e4c135e73a51790bc07
Carolina Coimbra <Carolina Coimbra>
2016-06-29 22:51:36 -0300

    fim

--- map.js
+++ map.js
@@ -14,7 +14,14 @@ function create_map(div)
 
 	if(div == "#squareOne")
 	{
-		// Define map size on screen 
+
+		var transferNorte = [370159054.26, 390967669.56, 146380128.81, 7340075.00, 258300516.16];
+		var transferNordeste = [1363051601.39, 2549371606.36, 981307028.50, 105338747.12, 1162830459.74];
+		var transferCentroOeste = [1582992827.55, 693085249.17, 817229036.07, 79131410.09, 902410462.01];
+		var transferSudeste = [673591156.34, 2438929485.17, 926288675.64, 633480237.35, 2897810237.72];
+		var transferSul = [785852305.81, 1005582872.08, 493142364.54, 271151625.19, 798338778.34];
+		var transferSum = [4775646945.35, 7077936882.34, 3364347233.56, 1096442094.75, 6019690453.97];
+	
 		var width = 350,
 			height = 350;
 
@@ -26,7 +33,7 @@ function create_map(div)
 
 		var g = svg.append("g");
 
-		// Align center of Brazil to center of map
+		
 		var projection = d3.geo.mercator()
 		  .scale(350)
 		  .center([-52, -15])
@@ -35,7 +42,7 @@ function create_map(div)
 		var path = d3.geo.path()
 		  .projection(projection);
 
-		// Load data (asynchronously)
+	
 		d3_queue.queue()
 			.defer(d3.json, "./br_states.json")
 			.await(ready);
@@ -54,7 +61,7 @@ function create_map(div)
 			  .data(states.features)
 			  .enter()
 			  .append("path")
-			  .attr("class", function(d) { //POR ENQUANTO ISSO É INÚTIL PORQUE AS CORES SÃO AS MESMAS
+			  .attr("class", function(d) { 
 				if (d.id == 'AC' || d.id == 'AM' || d.id == 'AP'|| d.id == 'PA' || d.id == 'RO' || d.id == 'RR' || d.id == 'TO') {
 				  return "subunitNorte";
 				} 
@@ -205,14 +212,13 @@ function create_map(div)
 					}})
 				.attr("d", path);
 
-				//Draw state contour
+				
 				g.append("path")
 				.datum(states_contour)
 				.attr("d", path)
 				.attr("class", "state_contour");
 				
 
-				//ofsets plus width/height of transform, plsu 20 px of padding, plus 20 extra for tooltip offset off mouse
 				var offsetL = document.getElementById('#squareOne').offsetLeft+(width/2);
 				var offsetT =document.getElementById('#squareOne').offsetTop+(height/2);
 		}
@@ -224,6 +230,13 @@ function create_map(div)
 	
 	else if (div == "#squareOne2")
 	{
+		var transferNorte = [370159054.26, 390967669.56, 146380128.81, 7340075.00, 258300516.16];
+		var transferNordeste = [1363051601.39, 2549371606.36, 981307028.50, 105338747.12, 1162830459.74];
+		var transferCentroOeste = [1582992827.55, 693085249.17, 817229036.07, 79131410.09, 902410462.01];
+		var transferSudeste = [673591156.34, 2438929485.17, 926288675.64, 633480237.35, 2897810237.72];
+		var transferSul = [785852305.81, 1005582872.08, 493142364.54, 271151625.19, 798338778.34];
+		var transferSum = [4775646945.35, 7077936882.34, 3364347233.56, 1096442094.75, 6019690453.97];
+
 		var width = 350,
 			height = 350;
 
@@ -235,7 +248,7 @@ function create_map(div)
 
 		var g = svg.append("g");
 
-		// Align center of Brazil to center of map
+
 		var projection = d3.geo.mercator()
 		  .scale(350)
 		  .center([-52, -15])
@@ -244,7 +257,7 @@ function create_map(div)
 		var path = d3.geo.path()
 		  .projection(projection);
 
-		// Load data (asynchronously)
+		
 		d3_queue.queue()
 			.defer(d3.json, "./br_states.json")
 			.await(ready);
@@ -252,13 +265,13 @@ function create_map(div)
 		function ready(error, shp) {
 		  if (error) throw error;
 
-		  // Extracting polygons and contours
+		 
 		  var states = topojson.feature(shp, shp.objects.estados);
 		  var states_contour = topojson.mesh(shp, shp.objects.estados);
 		  var subunitChecked = 0;
 		  var colorsChecked = ["#29A03C", "#D2282B", "#FF800A", "#9562BE", "#2978B3"];
 
-		  // Draw states
+		 
 		  g.selectAll(".estado")
 			  .data(states.features)
 			  .enter()
@@ -283,23 +296,23 @@ function create_map(div)
 			  .style("opacity", function(d) {
 				if (d.id == 'AC' || d.id == 'AM' || d.id == 'AP'|| d.id == 'PA' || d.id == 'RO' || d.id == 'RR' || d.id == 'TO') {
 				  //transfer (YEAR, 'Norte');
-				  return 0.2+transferNorte[idYear]/transferSum[idYear];
+				  return 0.2+transferNorte[idYear2]/transferSum[idYear2];
 				} 
 				else if (d.id == 'AL' || d.id == 'BA' || d.id == 'CE' || d.id == 'MA' || d.id == 'PB' || d.id == 'PE' || d.id == 'PI'|| d.id == 'RN'|| d.id == 'SE') {
 				  //transfer (YEAR, 'Nordeste');
-				  return 0.2+transferNordeste[idYear]/transferSum[idYear];
+				  return 0.2+transferNordeste[idYear2]/transferSum[idYear2];
 				}
 				else if (d.id == 'DF' || d.id == 'GO' || d.id == 'MS' || d.id == 'MT') {
 				  //transfer (YEAR, 'Centro-Oeste');
-				  return 0.2+transferCentroOeste[idYear]/transferSum[idYear];
+				  return 0.2+transferCentroOeste[idYear2]/transferSum[idYear2];
 				}
 				else if (d.id == 'ES' || d.id == 'MG' || d.id == 'RJ'|| d.id == 'SP') {
 				  //transfer (YEAR, 'Sudeste');
-				  return 0.2+transferSudeste[idYear]/transferSum[idYear];
+				  return 0.2+transferSudeste[idYear2]/transferSum[idYear2];
 				}
 				else if (d.id == 'PR' || d.id == 'RS' || d.id == 'SC') {
 				  //transfer (YEAR, 'Sul');
-				  return 0.2+transferSul[idYear]/transferSum[idYear];
+				  return 0.2+transferSul[idYear2]/transferSum[idYear2];
 				}})
 			  
 			   .on("mouseover", function(d,i) {
@@ -308,29 +321,29 @@ function create_map(div)
 					var transfer;
 					if (d.id == 'AC' || d.id == 'AM' || d.id == 'AP'|| d.id == 'PA' || d.id == 'RO' || d.id == 'RR' || d.id == 'TO') {
 						subunitOver = "Norte";
-						transfer = transferNorteSubtitle[idYear];
+						transfer = transferNorteSubtitle[idYear2];
 					} 
 					else if (d.id == 'AL' || d.id == 'BA' || d.id == 'CE' || d.id == 'MA' || d.id == 'PB' || d.id == 'PE' || d.id == 'PI'|| d.id == 'RN'|| d.id == 'SE') {
 						subunitOver = "Nordeste";
-						transfer = transferNordesteSubtitle[idYear];
+						transfer = transferNordesteSubtitle[idYear2];
 					}
 					else if (d.id == 'DF' || d.id == 'GO' || d.id == 'MS' || d.id == 'MT') {
 						subunitOver =  "Centro-Oeste";
-						transfer = transferCentroOesteSubtitle[idYear];
+						transfer = transferCentroOesteSubtitle[idYear2];
 					}
 					else if (d.id == 'ES' || d.id == 'MG' || d.id == 'RJ'|| d.id == 'SP') {
 						subunitOver = "Sudeste";
-						transfer = transferSudesteSubtitle[idYear];
+						transfer = transferSudesteSubtitle[idYear2];
 					}
 					else if (d.id == 'PR' || d.id == 'RS' || d.id == 'SC') {
 						subunitOver = "Sul";
-						transfer = transferSulSubtitle[idYear];
+						transfer = transferSulSubtitle[idYear2];
 					}
 			  
 				tooltip
 					.classed("hidden", false)
 					.attr("style", "left:"+(d3.event.pageX - 700)+"px;top:"+(d3.event.pageY - 100)+"px")
-					.html("<strong>Year: "+Year[idYear]+"<br/>"+"</strong>"+"<strong>Subunit: "+subunitOver+"<br/>"+"</strong>"+"<strong>Transfer: R$"+transfer+"<br/>"+"</strong>")
+					.html("<strong>Year: "+Year[idYear2]+"<br/>"+"</strong>"+"<strong>Subunit: "+subunitOver+"<br/>"+"</strong>"+"<strong>Transfer: R$"+transfer+"<br/>"+"</strong>")
 				})
 				.on("mouseout",  function(d,i) {
 					tooltip.classed("hidden", true)
@@ -413,15 +426,12 @@ function create_map(div)
 						global_reg2 = 4;
 					}})
 				.attr("d", path);
-
-				//Draw state contour
 				g.append("path")
 				.datum(states_contour)
 				.attr("d", path)
 				.attr("class", "state_contour");
 				
 
-				//ofsets plus width/height of transform, plsu 20 px of padding, plus 20 extra for tooltip offset off mouse
 				var offsetL = document.getElementById('#squareOne2').offsetLeft+(width/2);
 				var offsetT =document.getElementById('#squareOne2').offsetTop+(height/2);
 		}
@@ -429,13 +439,6 @@ function create_map(div)
 		
 	}
 
-
-var transferNorte = [370159054.26, 390967669.56, 146380128.81, 7340075.00, 258300516.16];
-var transferNordeste = [1363051601.39, 2549371606.36, 981307028.50, 105338747.12, 1162830459.74];
-var transferCentroOeste = [1582992827.55, 693085249.17, 817229036.07, 79131410.09, 902410462.01];
-var transferSudeste = [673591156.34, 2438929485.17, 926288675.64, 633480237.35, 2897810237.72];
-var transferSul = [785852305.81, 1005582872.08, 493142364.54, 271151625.19, 798338778.34];
-var transferSum = [4775646945.35, 7077936882.34, 3364347233.56, 1096442094.75, 6019690453.97];
 
 var transferNorteSubtitle = ["370.16M", "390.97M", "146.38M", "7.34M", "258.30M"];
 var transferNordesteSubtitle = ["1.36B", "2.55B", "981.31M", "105.34M", "1.16B"];
@@ -447,6 +450,13 @@ var transferSulSubtitle = ["785.85M", "1.01B", "493.14M", "271.15M", "798.34M"];
 
 
 function setYear(id){
+
+  	var transferNorte = [370159054.26, 390967669.56, 146380128.81, 7340075.00, 258300516.16];
+	var transferNordeste = [1363051601.39, 2549371606.36, 981307028.50, 105338747.12, 1162830459.74];
+	var transferCentroOeste = [1582992827.55, 693085249.17, 817229036.07, 79131410.09, 902410462.01];
+	var transferSudeste = [673591156.34, 2438929485.17, 926288675.64, 633480237.35, 2897810237.72];
+	var transferSul = [785852305.81, 1005582872.08, 493142364.54, 271151625.19, 798338778.34];
+	var transferSum = [4775646945.35, 7077936882.34, 3364347233.56, 1096442094.75, 6019690453.97];
 	
   idYear = id;
   reload_bars(idYear,global_reg, "#squareTwo");
@@ -461,16 +471,23 @@ function setYear(id){
 }
 
 function setYear2(id){
+
+	var transferNorte = [370159054.26, 390967669.56, 146380128.81, 7340075.00, 258300516.16];
+	var transferNordeste = [1363051601.39, 2549371606.36, 981307028.50, 105338747.12, 1162830459.74];
+	var transferCentroOeste = [1582992827.55, 693085249.17, 817229036.07, 79131410.09, 902410462.01];
+	var transferSudeste = [673591156.34, 2438929485.17, 926288675.64, 633480237.35, 2897810237.72];
+	var transferSul = [785852305.81, 1005582872.08, 493142364.54, 271151625.19, 798338778.34];
+	var transferSum = [4775646945.35, 7077936882.34, 3364347233.56, 1096442094.75, 6019690453.97];
 	
   idYear2 = id;
   reload_bars(idYear2,global_reg2, "#squareTwo2");
   reload_donut(idYear2,global_reg2 , "#squareThree2");
   reload_words(idYear2, global_reg2,"#squareFive2");
-  d3.selectAll(".subunitSul2").style("opacity", function(){return 0.2+transferSul[idYear]/transferSum[idYear];});
-  d3.selectAll(".subunitSudeste2").style("opacity", function(){return 0.2+transferSudeste[idYear]/transferSum[idYear];});
-  d3.selectAll(".subunitNorte2").style("opacity", function(){return 0.2+transferNorte[idYear]/transferSum[idYear];});
-  d3.selectAll(".subunitNordeste2").style("opacity", function(){return 0.2+transferNordeste[idYear]/transferSum[idYear];});
-  d3.selectAll(".subunitCentroOeste2").style("opacity", function(){return 0.2+transferCentroOeste[idYear]/transferSum[idYear];});  
+  d3.selectAll(".subunitSul2").style("opacity", function(){return 0.2+transferSul[idYear2]/transferSum[idYear2];});
+  d3.selectAll(".subunitSudeste2").style("opacity", function(){return 0.2+transferSudeste[idYear2]/transferSum[idYear2];});
+  d3.selectAll(".subunitNorte2").style("opacity", function(){return 0.2+transferNorte[idYear2]/transferSum[idYear2];});
+  d3.selectAll(".subunitNordeste2").style("opacity", function(){return 0.2+transferNordeste[idYear2]/transferSum[idYear2];});
+  d3.selectAll(".subunitCentroOeste2").style("opacity", function(){return 0.2+transferCentroOeste[idYear2]/transferSum[idYear2];});  
  
 }
 function alerta(){
